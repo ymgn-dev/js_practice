84617857f56137dcfbbe47aa6225aa7f789687e4
ymgn9314 <yamagen.private@gmail.com>
2022-01-30 19:49:30 +0900

    update

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 // Tips
 // 変数はlet, constで宣言
 // strictモードを使う
+'use strict'
 
 // jsではプライベートを表すのに_を付けない方がいい
 // また、変数の命名にはcamelCaseを使うのが好まれている
@@ -99,3 +100,48 @@ let obj = { name: 'Harry Smith', age: 42 }
 for (const key in obj) {
   console.log(`${key}: ${obj[key]}`)
 }
+
+let missingNumbers = [1, 2, , 4]
+missingNumbers[99] = 100
+for (const i in missingNumbers) {
+  console.log(`${i}: ${missingNumbers[i]}`)
+}
+
+// 例外処理
+try {
+  let parsed = parseInt('hoge')
+  if (!parsed) {
+    throw Error
+  }
+  console.log(parsed)
+} catch {
+  console.log('failed to parseInt.')
+}
+
+// 関数宣言
+function average(x, y) {
+  return (x + y) / 2
+}
+let result = average(6, 7)
+
+// 関数は値の1つでもあるため、変数に格納することもできる
+let f = average
+let result2 = f(6, 7)
+
+// 関数リテラル
+let multiBy10 = [0, 1, 2, 4].map(function (x) {
+  return x * 10
+})
+
+// アロー関数
+// アロー関数はfunctionsキーワードを使うよりも正規な振る舞いをする(this参照)
+// アロー関数を積極的に使うのが良い
+const arrowAverage = (x, y) => (x + y) / 2
+const arrowMultiBy10 = (x) => x * 10
+const dieToss = () => Math.trunc(Math.random() * 6) + 1
+
+// クロージャ
+/// タイマーを設定するsetTimeout関数
+let text = 'goodbye'
+setTimeout(() => console.log(text), 1000)
+text = 'hello'
